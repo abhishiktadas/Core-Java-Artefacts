e32bf8d8ce1f1124ac1474fe1633d48087750207
Abhishikta Das <95714870+abhishiktadas@users.noreply.github.com>
2024-11-04 19:28:19 +0530

    Nested IF in Java

---
+++ Nested_If.java
@@ -0,0 +1,26 @@
+package test;
+
+public class Nested_If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		if(a>5)
+		{
+			if(b>5)
+			{
+				if(c>5)
+				{
+					System.out.println("True");
+				}
+			}
+		}
+		else
+		{
+			
+		}
+}
+}
